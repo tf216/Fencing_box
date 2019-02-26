3f18e427411b05622381f90469a1fb408c3506f1
tf216 <tf216@skbl-422-005.ph.ic.ac.uk>
2019-02-26 11:18:46 +0000

    Working score, time and play/pause. No interrupts

--- Hit.asm
+++ Hit.asm
@@ -24,11 +24,22 @@ interupt_setup	    ;look at example code in presentation and can see only enable
 	bsf	INTCON2, RBPU
 	bcf	RCON, 7
 	bsf	INTCON, RBIE	;Enable PORTB interrupts
+	;bsf	INTCON, TMR0IE	;Enable Timer interupt
 	bsf	INTCON, GIE	;Enable all interrupts
+	
+	;bsf
 	return
+	
+
 	
 hit_left
 	bsf	hit,1	    ;note that left has hit
+	movlw	0xFF
+	call	ms_delay    ;delay where other fencer can hit
+	movlw	0xFF
+	call	ms_delay    ;delay where other fencer can hit
+	movlw	0xFF
+	call	ms_delay    ;delay where other fencer can hit
 	movlw	0xFF
 	call	ms_delay    ;delay where other fencer can hit
 	bsf	hit,0	    ;prevent other fencer's hit from registering
@@ -37,16 +48,22 @@ hit_right
 	bsf	hit,2	    ;note that right has hit
 	movlw	0xFF
 	call	ms_delay    ;delay where other fencer can hit
+	movlw	0xFF
+	call	ms_delay    ;delay where other fencer can hit
+	movlw	0xFF
+	call	ms_delay    ;delay where other fencer can hit
+	movlw	0xFF
+	call	ms_delay    ;delay where other fencer can hit
 	bsf	hit,0	    ;prevent other fencer's hit from registering
 	goto	lights
 
 lights	
-	movlw	0xFF
-	movwf	PORTE
-;	btfsc	hit,1
-;	bsf	PORTE,0
-;	btfsc	hit,2
-;	bsf	PORTE,7
+;	movlw	0xFF
+;	movwf	PORTE
+	btfsc	hit,1
+	bsf	PORTE,0
+	btfsc	hit,2
+	bsf	PORTE,7
 	movlw	0xFF
 	call	ms_delay
 	movlw	0xFF
