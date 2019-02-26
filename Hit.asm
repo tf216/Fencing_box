a74f625515863f4ff64d89ede4f03c70dc4b46c3
tf216 <tf216@skbl-422-005.ph.ic.ac.uk>
2019-02-26 15:27:31 +0000

    Working score, time and play/pause. No interrupts

--- Hit.asm
+++ Hit.asm
@@ -9,77 +9,84 @@
 
 	
 hit_int	code	0x0008
-	btfss	INTCON, RBIF	;check if it was an interrupt caused by PORTB
-	retfie	1		;rturns to where the interrupt has been called from
-	call	score		;call the scoring systemn and stop the time	
-	bcf	INTCON, RBIF	;Clear flag bit
-	retfie	1
+	btfsc	INTCON, RBIF	;check if it was an interrupt caused by PORTB
+	call	test		;returns to where the interrupt has been called from
+				;call the scoring systemn and stop the time	
+;	bcf	INTCON, RBIF	;Clear flag bit
+;	bcf	INTCON,	TMR0IF	;Clear timer flag
+;	bcf	INTCON, TMR0IE
+	incf	LATE
+	bcf	INTCON,	TMR0IF	;Clear timer flag
+	
+
+	
+	movlw	0xF6		;start a timer
+	;movwf	0x00
+	movwf	TMR0H
+	movlw	0xB8
+	;movwf	0x00
+	movwf	TMR0L
+	
+	retfie	FAST
 
 	
 	
 hit code
-	
+ 	
 interupt_setup	    ;look at example code in presentation and can see only enables certain bits
 		    ;doesn't reset the whole SFR
+	bcf	INTCON, RBIF
+	bcf	INTCON,	TMR0IF
 	bsf	INTCON2, RBPU
 	bcf	RCON, 7
 	bsf	INTCON, RBIE	;Enable PORTB interrupts
-	;bsf	INTCON, TMR0IE	;Enable Timer interupt
-	bsf	INTCON, GIE	;Enable all interrupts
+	bsf	INTCON, GIE	;Enable all interrupts	
 	
-	;bsf
+	movlw	b'10000111'	;set prescaler for 16 bit, prescaler to 64KHz
+	movwf	T0CON		
 	return
 	
-
+hit_check
+	bcf	INTCON,TMR0IF
+	;movlw	0xF6		;start a timer
+	movwf	0x00
+	movwf	TMR0H
+	;movlw	0xB8
+	movwf	0x00
+	movwf	TMR0L
+	bsf	INTCON, TMR0IE
+    
+	btfsc	PORTB,4
+	bsf	hit,1
+	btfsc	PORTB,5
+	bsf	hit,2
+	return
+	
 	
 hit_left
 	bsf	hit,1	    ;note that left has hit
-	movlw	0xFF
-	call	ms_delay    ;delay where other fencer can hit
-	movlw	0xFF
-	call	ms_delay    ;delay where other fencer can hit
-	movlw	0xFF
-	call	ms_delay    ;delay where other fencer can hit
-	movlw	0xFF
-	call	ms_delay    ;delay where other fencer can hit
-	bsf	hit,0	    ;prevent other fencer's hit from registering
-	goto	lights
+	retfie	1
+
 hit_right
 	bsf	hit,2	    ;note that right has hit
-	movlw	0xFF
-	call	ms_delay    ;delay where other fencer can hit
-	movlw	0xFF
-	call	ms_delay    ;delay where other fencer can hit
-	movlw	0xFF
-	call	ms_delay    ;delay where other fencer can hit
-	movlw	0xFF
-	call	ms_delay    ;delay where other fencer can hit
-	bsf	hit,0	    ;prevent other fencer's hit from registering
-	goto	lights
+	retfie	1
 
 lights	
-;	movlw	0xFF
-;	movwf	PORTE
+
 	btfsc	hit,1
 	bsf	PORTE,0
 	btfsc	hit,2
 	bsf	PORTE,7
-	movlw	0xFF
-	call	ms_delay
-	movlw	0xFF
-	call	ms_delay
-	movlw	0xFF
-	call	ms_delay
-	movlw	0xFF
-	call	ms_delay
-	movlw	0xFF
-	call	ms_delay
-	movlw	0xFF
-	call	ms_delay
+
 	movlw	0x00
 	movwf	hit
 	movwf	PORTE
+	call	score
 	return
+	
+test	bsf	INTCON, TMR0IE
+	retfie	1
+	
 	
 
 	
